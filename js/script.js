b228a9ca7ab099783041a33508158906baed9166
dmitryzolot <zoldiman@gmail.com>
2020-03-25 18:43:23 +0200

    итоговая проверка Nerds

--- js/script.js
+++ js/script.js
@@ -1,13 +1,50 @@
 var popup = document.querySelector('.popup');
 var popupClose = popup.querySelector('.close-cross');
 var popupOpen = document.querySelector('.writeus-button');
+var popupForm = popup.querySelector('form');
+var formSubmit = popup.querySelector('.popup-submit');
 
-popupOpen.addEventListener('click', function(evt) {
+var nameInput = popup.querySelector('#name');
+var emailInput = popup.querySelector('#email');
+var textArea = popup.querySelector('#text');
+
+
+function openButtonClickHandler (evt) {
   evt.preventDefault();
   popup.classList.add('popup-open');
+  nameInput.focus();
+};
+
+function closeButtonClickHandler () {
+  popup.classList.remove('popup-open');
+  popup.classList.remove('popup-error');
+  popupForm.reset();
+};
+
+popupOpen.addEventListener('click', function(evt) {
+  openButtonClickHandler(evt);
 });
 
-popupClose.addEventListener('click', function() {
-  popup.classList.remove('popup-open');
+popupClose.addEventListener('click', closeButtonClickHandler);
+
+popupClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 13) {
+    closeButtonClickHandler();
   }
-);
+});
+
+document.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 27) {
+    closeButtonClickHandler();
+  }
+});
+
+function checkInputLength () {
+  if (emailInput.value === '' || nameInput.value.length === '' || textArea.value === '') {
+    popup.classList.remove('popup-error');
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add('popup-error');
+  }
+};
+
+formSubmit.addEventListener('click', checkInputLength);
